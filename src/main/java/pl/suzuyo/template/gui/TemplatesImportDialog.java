a8e96ee4c25af23b8ef557105c8f262db4eed1c3
Damian <damian2006a@gmail.com>
2020-04-03 22:24:47 +0200

    Change import layout

--- src/main/java/pl/suzuyo/template/gui/TemplatesImportDialog.java
+++ src/main/java/pl/suzuyo/template/gui/TemplatesImportDialog.java
@@ -1,6 +1,7 @@
 package pl.suzuyo.template.gui;
 
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.JBColor;
 import com.sun.istack.Nullable;
 
 import javax.swing.*;
@@ -8,23 +9,28 @@ import java.awt.*;
 
 public class TemplatesImportDialog extends DialogWrapper {
 
-    private String text;
+    private String templateName;
 
-    public TemplatesImportDialog(String text) {
+    public TemplatesImportDialog(String templateName) {
         super(true);
         setTitle("Question");
         setResizable(false);
-        this.text = text;
+        this.templateName = templateName;
         init();
     }
 
     @Nullable
     @Override
     protected JComponent createCenterPanel() {
-        JPanel dialogPanel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel(text);
-        label.setPreferredSize(new Dimension(0, 40));
-        dialogPanel.add(label, BorderLayout.CENTER);
+        JPanel dialogPanel = new JPanel(new FlowLayout());
+        JLabel label1 = new JLabel("Template about name");
+        JLabel templateLabel = new JLabel(templateName);
+        templateLabel.setForeground(JBColor.RED);
+        JLabel label2 = new JLabel("already exists. Do you want to replace it?");
+        dialogPanel.setPreferredSize(new Dimension(0, 30));
+        dialogPanel.add(label1);
+        dialogPanel.add(templateLabel);
+        dialogPanel.add(label2);
         return dialogPanel;
     }
 }
